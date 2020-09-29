136e8da5d6daa15d8db24b12d87ffda75702ea7b
Jiequan Zhang <jiequanzhang@DNa80cbec.SUNet>
2020-09-29 15:10:06 -0700

    files for ploting

---
+++ run.sh
@@ -0,0 +1,50 @@
+
+
+
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 1
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 2
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 3
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 4
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 5
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 6
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 7
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 8
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 9
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 10
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 11
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 12
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 13
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 14
+# python -m gym_SmartPrimer.examples.exampleBaselineSherry --seed 15
+
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 1
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 2
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 3
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 4
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 5
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 6
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 7
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 8
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 9
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 10
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 11
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 12
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 13
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 14
+# python -m gym_SmartPrimer.examples.exampleHintSherry --seed 15
+
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 1
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 2
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 3
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 4
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 5
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 6
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 7
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 8
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 9
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 10
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 11
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 12
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 13
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 14
+python -m gym_SmartPrimer.examples.examplePPOManualSherry --seed 15
